5f401e094fdd1d93897acbd6d97102e16b48da41
Ben Chen <chungben@hotmail.com>
2023-07-09 22:30:21 +0800

    20230709

--- js/script.js
+++ js/script.js
@@ -495,7 +495,7 @@ function startWrong() {
 	var base_left_filename = "./";
 
 	if (curWeb.includes("computer")) {
-		base_left_filename = "../computer/";
+		base_left_filename = "./computer/";
 	}
 
 	var base_left_filename =
@@ -698,7 +698,7 @@ function getQuestions() {
 	var base_left_filename = "./";
 
 	if (curWeb.includes("computer")) {
-		base_left_filename = "../computer/";
+		base_left_filename = "./computer/";
 	}
 
 	var base_left_filename =
@@ -2187,7 +2187,7 @@ function startAudio(curQuiz) {
 	var base_left_filename = "./";
 
 	if (curWeb.includes("computer")) {
-		base_left_filename = ".//computer";
+		base_left_filename = "./computer/";
 	}
 
 	var base_left_filename =
